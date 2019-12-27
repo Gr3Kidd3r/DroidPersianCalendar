e433812d36420a4d71c77d4075cac5c5483bd4a4
Hamidreza Bayat <Hamidrezabayat76@gmail.com>
2019-12-27 20:37:34 +0330

    Use single threadExecutor for UpdateWorker couroutine

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/service/UpdateWorker.kt
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/service/UpdateWorker.kt
@@ -6,21 +6,23 @@ import androidx.work.WorkerParameters
 import com.byagowi.persiancalendar.utils.setChangeDateWorker
 import com.byagowi.persiancalendar.utils.update
 import com.byagowi.persiancalendar.utils.updateStoredPreference
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.withContext
+import java.util.concurrent.Executors
 
 class UpdateWorker(val context: Context, workerParams: WorkerParameters) :
     CoroutineWorker(context, workerParams) {
 
-    override suspend fun doWork(): Result = withContext(Dispatchers.Main) {
-        try {
-            setChangeDateWorker(context)
-            updateStoredPreference(applicationContext)
-            update(applicationContext, true)
-            Result.success()
-        } catch (e: Throwable) {
-            e.printStackTrace()
-            Result.failure()
+    override suspend fun doWork(): Result =
+        withContext(Executors.newSingleThreadExecutor().asCoroutineDispatcher()) {
+            try {
+                setChangeDateWorker(context)
+                updateStoredPreference(applicationContext)
+                update(applicationContext, true)
+                Result.success()
+            } catch (e: Throwable) {
+                e.printStackTrace()
+                Result.failure()
+            }
         }
-    }
 }
